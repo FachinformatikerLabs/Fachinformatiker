17883ae335514ff78af0f9f96be21c340236e5f1
nyxb <contact@nyxb.email>
2023-10-19 13:17:14 +0200

    🔧 chore(gitignore): add pnpm-lock.yaml to .gitignore to prevent accidental commit of package lock file ✏️ refactor(app.config.ts): update app title, description, and base URL to reflect new project focus 🔥 remove(app.config.ts): comment out Nuxt social link as it's not relevant to the new project focus

--- app.config.ts
+++ app.config.ts
@@ -1,18 +1,18 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'Fachinformatiker',
+    description: 'Fachinformatiker" ist eine Online-Plattform für die Prüfungsvorbereitung, die dich gezielt auf die Fachinformatiker-Prüfung vorbereitet. Durch eine Vielzahl an Ressourcen und Übungsmaterialien ermöglicht sie dir, dich effektiv und flexibel auf deine bevorstehende Prüfung vorzubereiten.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
 
     socials: {
       twitter: 'nyxb0',
       github: 'nyxb',
 
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      },
+      // nuxt: {
+      //   label: 'Nuxt',
+      //   icon: 'simple-icons:nuxtdotjs',
+      //   href: 'https://nuxt.com'
+      // },
 
       youtube: 'https://youtube.com/@nyxb_0'
     },
@@ -23,7 +23,7 @@ export default defineAppConfig({
       repo: 'Fachinformatiker',
       owner: 'nyxb',
       edit: true,
-      baseUrl: ''
+      baseUrl: 'https://github.com/nyxb/Fachinformatiker/blob/main'
     },
 
     aside: {
