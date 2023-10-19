072346b02933aa52454d2c5a818ce8346ac6df2a
Dennis Ollhoff <contact@nyxb.email>
2023-10-19 13:06:13 +0200

    github settings

--- app.config.ts
+++ app.config.ts
@@ -3,36 +3,52 @@ export default defineAppConfig({
     title: 'Docus',
     description: 'The best place to start your documentation.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
+      twitter: 'nyxb0',
+      github: 'nyxb',
+
       nuxt: {
         label: 'Nuxt',
         icon: 'simple-icons:nuxtdotjs',
         href: 'https://nuxt.com'
-      }
+      },
+
+      youtube: 'https://youtube.com/@nyxb_0'
     },
+
     github: {
-      dir: '.starters/default/content',
+      dir: '/content',
       branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
+      repo: 'Fachinformatiker',
+      owner: 'nyxb',
+      edit: true,
+      baseUrl: ''
     },
+
     aside: {
       level: 0,
       collapsed: false,
       exclude: []
     },
+
     main: {
       padded: true,
       fluid: true
     },
+
     header: {
       logo: true,
       showLinkIcon: true,
       exclude: [],
       fluid: true
+    },
+
+    footer: {
+      credits: {
+        text: 'Powered by Nyxb',
+        href: 'https://nyxb.blog'
+      }
     }
   }
 })
