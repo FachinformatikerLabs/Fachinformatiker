99e7b3e42f7c5fa95be593932b7fd0d8c01ae5a8
nyxb <contact@nyxb.email>
2023-11-04 21:01:44 +0100

    🔄 refactor(app.config.ts): update app configuration to reflect new project details 🔄 refactor(nuxt.config.ts): switch from docus to docux for better project compatibility 🔧 fix(package.json): update package details and dependencies to match new project requirements

--- app.config.ts
+++ app.config.ts
@@ -1,49 +1,43 @@
 export default defineAppConfig({
-  docus: {
-    title: 'Fachinformatiker',
-    description: 'Fachinformatiker" ist eine Online-Plattform für die Prüfungsvorbereitung, die dich gezielt auf die Fachinformatiker-Prüfung vorbereitet. Durch eine Vielzahl an Ressourcen und Übungsmaterialien ermöglicht sie dir, dich effektiv und flexibel auf deine bevorstehende Prüfung vorzubereiten.',
-    image: 'https://raw.githubusercontent.com/FachinformatikerLabs/Fachinformatiker/main/public/fachinformatiker-og.png',
-
-    socials: {
-      discord: 'http://nyxb.chat',
-      twitter: 'nyxb0',
-      github: 'nyxb',
-      youtube: 'https://youtube.com/@nyxb_0'
-    },
-
-    github: {
-      dir: '/content',
-      branch: 'main',
-      repo: 'Fachinformatiker',
-      owner: 'nyxb',
-      edit: true,
-      baseUrl: 'https://github.com/'
-    },
-
-    aside: {
-      level: 0,
-      collapsed: false,
-      exclude: []
-    },
-
-    main: {
-      padded: true,
-      fluid: true
-    },
-
-    header: {
-      logo: true,
-      showLinkIcon: true,
-      exclude: [],
-      fluid: true
-    },
-
-    footer: {
-      credits: {
-        icon: '',
-        text: 'Powered by Nyxb',
-        href: 'https://nyxb.blog'
-      }
-    }
-  }
+   docux: {
+      title: 'Fachinformatiker',
+      description: 'Fachinformatiker" ist eine Online-Plattform für die Prüfungsvorbereitung, die dich gezielt auf die Fachinformatiker-Prüfung vorbereitet. Durch eine Vielzahl an Ressourcen und Übungsmaterialien ermöglicht sie dir, dich effektiv und flexibel auf deine bevorstehende Prüfung vorzubereiten.',
+      image: 'https://github.com/nyxb/docux/blob/main/.github/images/docux_repo_picture.png',
+      socials: {
+         // twitter: 'nyxb0',
+         github: 'FachinformatikerLabs/Fachinformatiker',
+         discord: 'https://fachinformatiker.site/discord',
+      },
+      github: {
+         dir: 'content',
+         branch: 'main',
+         repo: 'Fachinformatiker',
+         owner: 'FachinformatikerLabs',
+         edit: true,
+      },
+      aside: {
+         level: 1,
+         collapsed: false,
+         exclude: [],
+         navigation: true,
+      },
+      main: {
+         padded: true,
+      },
+      header: {
+         title: '',
+         logo: true,
+         showLinkIcon: true,
+         exclude: [],
+         navigation: false,
+      },
+      footer: {
+         iconLinks: [
+            // {
+            //    href: 'https://nyxb.nexus',
+            //    icon: 'simple-icons:web',
+            // },
+         ],
+      },
+   },
 })
